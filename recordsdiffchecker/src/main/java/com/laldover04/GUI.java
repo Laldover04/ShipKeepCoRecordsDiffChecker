49487ee24d0108937ce0380e7d0f5bddd4a8e316
Luke Aldover <122496790+Laldover04@users.noreply.github.com>
2025-01-15 14:59:41 -0700

    Copied in code from my files
    Copied in code from my other files from before getting maven working.

---
+++ recordsdiffchecker/src/main/java/com/laldover04/GUI.java
@@ -0,0 +1,137 @@
+package com.laldover04;
+
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+
+public class GUI implements ActionListener {
+    private int windowWidth = 600;
+    private int windowHeight = 400;
+
+
+    private JFrame frame;
+    private JPanel panel;
+
+
+    private JLabel resultLabel;
+    private JLabel titleLabel;
+    private JLabel transactionLookupLabel;
+    private JLabel spaInputLabel;
+    private JLabel serviceCodeInputLabel;
+
+
+    private JButton resetButton;
+    private JButton searchButton;
+
+
+    private JTextField spaInput;
+    private JTextField serviceCodeInput;
+
+
+    public GUI() {
+        // Initial setup
+        panel = new JPanel();
+        frame = new JFrame();
+        frame.setSize(windowWidth, windowHeight);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+
+        //
+        panel.setLayout(null);
+
+
+        // Title
+        titleLabel = new JLabel("Ship Keep Co Records");
+        titleLabel.setFont(new Font("Serif", Font.BOLD, 18));
+        titleLabel.setBounds(windowWidth / 3, 0, 200, 25);
+        panel.add(titleLabel);
+
+
+        // Search Area Description
+        transactionLookupLabel = new JLabel("Transaction Record Lookup");
+        transactionLookupLabel.setBounds(windowWidth / 10 - 17, 50, 200, 25);
+        transactionLookupLabel.setFont(new Font("Arial", Font.BOLD, 14));
+        panel.add(transactionLookupLabel);
+
+
+        // SPA label
+        spaInputLabel = new JLabel("SPA");
+        spaInputLabel.setBounds(windowWidth / 10, 90, 300, 25);
+        panel.add(spaInputLabel);
+
+
+        // SPA input
+        spaInput = new JTextField(20);
+        spaInput.setBounds(windowWidth / 10, 115, 165, 25);
+        panel.add(spaInput);
+
+
+        // Service Code label
+        serviceCodeInputLabel = new JLabel("Service Code");
+        serviceCodeInputLabel.setBounds(windowWidth / 10, 150, 300, 25);
+        panel.add(serviceCodeInputLabel);
+
+
+        // Service Code input
+        serviceCodeInput = new JTextField(20);
+        serviceCodeInput.setBounds(windowWidth / 10, 175, 165, 25);
+        panel.add(serviceCodeInput);
+
+
+        // Search Button
+        searchButton = new JButton("search");
+        searchButton.setBounds(windowWidth / 10 + 85, 210, 80, 25);
+        searchButton.addActionListener(this);
+        panel.add(searchButton);
+
+
+        // Reset Button
+        resetButton = new JButton("reset");
+        resetButton.setBounds(windowWidth / 10, 210, 80, 25);
+        resetButton.addActionListener(this);
+        panel.add(resetButton);
+
+
+        // Lookup Results label
+        resultLabel = new JLabel("search results");
+        resultLabel.setBounds(windowWidth / 2, 115, 300, 25);
+        panel.add(resultLabel);
+
+
+        frame.setVisible(true);
+
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+
+        // resets resultLabel and inputs on click
+        if (e.getSource() == resetButton) {
+            spaInput.setText("");
+            serviceCodeInput.setText("");
+            resultLabel.setText("");
+        } else if (e.getSource() == searchButton) {
+            // sets result label to the input fields will have to be changed to use a
+            // XLSXReader object to look up the row information.
+            resultLabel.setText(spaInput.getText() + " " + serviceCodeInput.getText());
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new GUI();
+    }
+
+};
---
+++ recordsdiffchecker/src/main/java/com/laldover04/XLSXreader.java
@@ -0,0 +1,73 @@
+package com.laldover04;
+
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class XLSXreader {
+    public static void main(String[] args) {
+
+
+        File file = new File("ServiceCodes_TAR.xlsx");
+        try (FileInputStream fileStream = new FileInputStream(file)) {
+            System.out.println("success");
+            System.out.println("FileName: " + file.getName());
+            System.out.println("Readable: " + file.canRead());
+
+
+            // workbook
+            Workbook wb = new XSSFWorkbook(fileStream);
+
+
+            // sheet
+            Sheet sheet = wb.getSheetAt(0);
+
+
+            // iterate on rows
+            Iterator<Row> rowIt = sheet.iterator();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    // private static XSSFSheet getSheet(String fileName) {
+
+
+    // }
+}
+
+
+// while (rowIt.hasNext()) {
+// Row row = rowIt.next();
+// System.out.println("\n row: " + row.getRowNum());
+// if (row.getRowNum() > 10) {
+// break;
+// }
+
+
+// // iterate on cells
+// Iterator<Cell> cellIt = row.cellIterator();
+// while (cellIt.hasNext()) {
+// Cell cell = cellIt.next();
+// System.out.println(cell.getNumericCellValue());
+// }
+
+
+// }
